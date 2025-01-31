5c23523e7a032aa952a015dc2641da25d92828c8
Muhammad Darda Fathammubienaa <muhammaddarda733@gmail.com>
2025-01-31 15:10:54 +0700

    update

--- js/project.js
+++ js/project.js
@@ -1,185 +1,186 @@
-import {data} from "/js/data-project.js";
-
-gsap.registerPlugin(ScrollTrigger);
-
-const lenis = new Lenis();
-lenis.on("scroll", ScrollTrigger.update);
-gsap.ticker.add((time) => {
-    lenis.raf(time * 1000);
-});
-gsap.ticker.lagSmoothing(0);
-
-const pinnedSection = document.querySelector(".pinned");
-const progressBar =  document.querySelector(".progress");
-const pinnedHeight = window.innerHeight * 10;
-const images = gsap.utils.toArray(".img");
-
-function animateImageEntry(img){
-    gsap.fromTo(
-        img, {
-            scale: 1.25,
-            clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
-            opacity: 0
-        },{
-            scale:1,
-            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-            opacity:1,
-            duration: 1,
-            ease: "power2.inOut"
-        }
+document.addEventListener("DOMContentLoaded", ()=>{
+    gsap.registerPlugin(CustomEase);
+    CustomEase.create(
+        "hop",
+        "M0, 0 C0.354,0 0.464,0.133 0.498,0.502 0.532,0.872 0.651,1 1,1"
     );
 
-    gsap.fromTo(
-        img.querySelector("img"),
-        {
-            filter: "contrast(2), brightness(10)"
-        },
-        {
-            filter: "contrast(1), brightness(1)",
-            duration:1,
-            ease:"power2.inOut"
-        }
-    );
-}
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+    const links = document.querySelectorAll('.link');
+    const socialLinks = document.querySelectorAll('.socials a');
+    let isAnimating = false;
 
-function animateImageExitForward(img){
-    gsap.to(img,{
-        scale: 0.5,
-        opacity: 0,
-        duration: 1,
-        ease: "power2.inOut"
-    });
-}
-
-function animateImageExitReverse(img){
-    gsap.to(img,{
-        scale: 1.25,
-        clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
-        duration: 1,
-        ease: "power2.inOut"
-    });
-    gsap.to(
-        img.querySelector("img"),
-        {
-            filter: "contrast(2), brightness(10)",
-            duration: 1,
-            ease:"power2.inOut"
+    const splitTextIntoSpans = (selector) => {
+        let elements = document.querySelectorAll(selector);
+        elements.forEach((element) => {
+            let text = element.innerText;
+            let splitText = text
+                .split("")
+                .map(function (char){
+                    return `<span>${char == " " ? "&nbsp;&nbsp" : char}</span>`;
+                })
+                .join("");
+            element.innerHTML = splitText;
+            console.log(element.innerHTML);
         });
-}
+    };
+    splitTextIntoSpans(".header h3");
 
-function updateInfoContent(index){
-    const infoItems = document.querySelectorAll(".info > div p");
-    const link = document.querySelector(".info .link a");
+    menuToggle.addEventListener("click",() => {
+        if(isAnimating) return;
 
-    infoItems.forEach((item) => (item.innerHTML = ""));
-    link.setAttribute("href", "#");
+        if(menuToggle.classList.contains("closed")){
+            menuToggle.classList.remove("closed");
+            menuToggle.classList.add("opened");
 
-    const item = data[index];
-    const contentArray = [item.title, item.tagline, item.year, item.tag];
+            isAnimating = true;
 
-    infoItems.forEach((element, i)=>{
-        if( 1 < 4){
-            const letters = contentArray[i].split("");
-            letters.forEach((letter, index) => {
-                const span = document.createElement("span");
-                span.textContent = letter;
-                span.style.opacity = 0;
-                element.appendChild(span);
-
-                gsap.to(span, {
-                    opacity: 1,
-                    duration: 0.1,
-                    delay: 0.03 * index,
-                    ease:"power1.inOut"
-
+            gsap.to(menu, {
+                clipPath: "polygon(0% 0%,100% 0%,100% 100%,0% 100%)",
+                ease: "hop",
+                duration: 1.5,
+                onStart:() => {
+                    menu.style.pointerEvents = "all";
+                },
+                onComplete: () => {
+                    isAnimating = false;
+                },
             });
-          });
+            gsap.to(links, {
+                y:0,
+                opacity:1,
+                stagger: 0.1,
+                delay: 0.85,
+                duration: 1,
+                ease: "power3.out",
+            });
+            gsap.to(socialLinks, {
+                y:0,
+                opacity:1,
+                stagger: 0.05,
+                delay: 0.85,
+                duration: 1,
+                ease: "power3.out",
+            });
+            gsap.to(".video-wrapper", {
+                clipPath: "polygon(0% 0%,100% 0%,100% 100%,0% 100%)",
+                ease: "hop",
+                delay: 0.5,
+                duration: 1.5,
+            });
+            gsap.to(".header h3 span", {
+                rotateY: 0,
+                stagger: 0.05,
+                delay: 0.75,
+                duration: 1.5,
+                ease: "power4.out",
+            });
+            gsap.to(".header h3 span", {
+                y:0,
+                scale: 1,
+                stagger:  0.05,
+                delay: 0.5,
+                duration: 1,
+                ease: "power4.out",
+            })
+        }else{
+            menuToggle.classList.remove("opened");
+            menuToggle.classList.add("closed");
+
+            isAnimating = true;
+
+            gsap.to(menu, {
+                clipPath: "polygon(0% 0%,100% 0%,100% 0%,0% 0%)",
+                ease: "hop",
+                duration:1.5,
+                onComplete: () =>{
+                    menu.style.pointerEvents = "none";
+                    gsap.set(menu,{
+                        clipPath: "polygon(0% 100%,100% 100%,100% 100%,0% 100%)",
+                    });
+                    gsap.set(links, {y: 30, opacity: 0});
+                    gsap.set(socialLinks, {y: 30, opacity: 0});
+                    gsap.set(".video-wrapper", {
+                        clipPath: "polygon(0% 100%,100% 100%,100% 100%,0% 100%)",
+                    });
+                    gsap.set(".header h3 span", {
+                        y: 500,
+                        rotateY: 90,
+                        scale: 0.75
+                    });
+
+                    isAnimating = false;
+                }
+            })
         }
-    });
-    link.setAttribute("href", item.link);
-
-    const linkText = link.textContent;
-    link.innerHTML = "";
-    linkText.split("").forEach((letter, index) => {
-        const span = document.createElement("span");
-        span.textContent = letter;
-        span.style.opacity = 0;
-        link.appendChild(span);
-
-        gsap.to(span, {
-            opacity: 1,
-            duration: 0.1,
-            delay: 0.03 * index,
-            ease:"power1.inOut"
-
-     });
     })
-}
+});
+AOS.init();
+// carousel
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    speed: 600,
 
-updateInfoContent(0);
-animateImageEntry(images[0]);
+    autoplay:{
+        delay:3000,
+        disableOnInteraction:false
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // responsive breakpoints
+    breakpoints: {
+        0:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        }
+    },
+    effect: "coverflow",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier:1,
+    },
+    // effect: 'coverflow',
+    // coverflowEffect: {
+    //     rotate: 10,
+    //     stretch: 0,
+    //     depth: 100,
+    //     modifier: 1,
+    //     slideShadows : true,
+    // },
 
-let lastCycle = 0;
-
-ScrollTrigger.create({
-    trigger: pinnedSection,
-    start: 'top top',
-    end: `+=${pinnedHeight} * 2`,
-    pin: true,
-    pinSpacing: true,
-    scrub: 0.1,
-    onUpdate: (self) => {
-        const totalProgress = self.progress * 5;
-        const currentCycle = Math.floor(totalProgress);
-        const cycleProgress = (totalProgress % 1) * 100;
-
-        if(currentCycle < images.length){
-            const currentImage = images[currentCycle];
-            const scale = 1 - (0.25 * cycleProgress) / 100;
-            gsap.to(currentImage, {
-                scale: scale,
-                duration:0.1,
-                overwrite: "auto"
-
+    speed: 600, // Kecepatan animasi
+    parallax: true,
+    on: {
+        slideChangeTransitionEnd: function(){
+            document.querySelectorAll('.swiper-slide').forEach(slide => {
+                slide.classList.remove('active-slide')
             });
-            if(currentCycle !== lastCycle){
-                if(self.direction > 0){
-                    if(lastCycle < images.length)
-                        animateImageExitForward(images[lastCycle]);
-                    if(currentCycle < images.length){
-                        animateImageEntry(images[currentCycle]);
-                        gsap.delayedCall(0.5, () => updateInfoContent(currentCycle));
-                    }              
-                }else{
-                    if(currentCycle< images.length){
-                        animateImageEntry(images[currentCycle]);
-                        gsap.delayedCall(0.5, () => updateInfoContent(currentCycle));
-                    }
-                    if(lastCycle < images.length){
-                        animateImageExitReverse(images[lastCycle]);
-                    }
-                    lastCycle = currentCycle;
-                }
-            }
-            if(currentCycle < 5){
-                gsap.to(progressBar,{
-                    height: `${cycleProgress}`,
-                    duration: 0.1,
-                    overwrite: true
-                });
 
-                if(cycleProgress < 1 && self.direction > 0){
-                    gsap.set(progressBar, { height: "0%"});
-                } else if(cycleProgress > 99 && self.direction < 0){
-                    gsap.set(progressBar, { height: "100%"});
-                }else{
-                    gsap.to(progressBar, { 
-                        height: self.direction > 0 ? "100%" : `${cycleProgress}%`,
-                        duration: 0.1,
-                        overwrite: true
-                    })
-                }
+            let activeSlide = document.querySelector('.swiper-slide-active');
+            if(activeSlide){
+                activeSlide.classList.add('active-slide');
             }
         }
     }
-})
+  });
